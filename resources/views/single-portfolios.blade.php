3dc5b347468af74c73994ad1d07c7175ddf9e891
Philip Rudy <me@philiparudy.com>
2022-03-18 13:54:04 -0400

    portfolio entry

--- resources/views/single-portfolios.blade.php
+++ resources/views/single-portfolios.blade.php
@@ -11,11 +11,13 @@
     <section class="services-banner section-alt">
         <div class="container">
             <div class="row">
+                <div class="services-header">
+                    <span class="header-tag">Portfolio</span>
+                    <h1 class="services-header-title">{{$title}}</h1>
+                </div>
+            </div>
+            <div class="row">
                 <div class="two-col">
-                    <div class="services-header">
-                        <span class="header-tag">Portfolio</span>
-                        <h1 class="services-header-title">{{$title}}</h1>
-                    </div>
                     <div class="services-video-wrapper">
                         <div class="services-video">
                             <img src="{{ $thumbnail_url }}" alt="{{$thumbnail_alt}}"/>
@@ -31,24 +33,40 @@
                             <p>Thus an investment into video marketing for accountants has a high return on Investment.
                             Some of the tangible benefits that you should see from a professional video production project include: </p>
                     </div>
-                    <div class="banner-list">
-                        <ul class="smaller checklist-board grid grid-cols-2 gap-2">
-                            <li>Lead conversion from referrals</li>
-                            <li>More time spent on your website</li>
-                            <li>Showcase partners’ expertise</li>
-                            <li>Build personal connections faster</li>
-                            <li>Higher ranking on search engines</li>
-                            <li>Stronger thought leadership</li>
-                        </ul>
-                    </div>
+
                 </div>
             </div>
         </div>
     </section>
 
+    <section class="section-alt-2">
+        <div class="container">
+            <div class="row center">
+                <div class="banner-list-wide">
+                    <ul class="smaller checklist-board grid grid-cols-2 gap-2">
+                        <li>Lead conversion from referrals</li>
+                        <li>More time spent on your website</li>
+                        <li>Showcase partners’ expertise</li>
+                        <li>Build personal connections faster</li>
+                        <li>Higher ranking on search engines</li>
+                        <li>Stronger thought leadership</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
     <section class="sizzle-reel-wide">
         <div class="container">
-            <div class="row">
+            <div class="row center">
+                <div class="header-two blue">
+                    <h2>
+                        Ask us why these are some of our favorite accounting videos.
+                    </h2>
+                </div>
+            </div>
+            <div class="row block">
                 <div class="grid grid-cols-3 gap-2">
                     @php
 
@@ -56,7 +74,6 @@
 if( have_rows('sizzle_reel') ):
 
                     @endphp
-                    <div class="services-sizzle-reel grid grid-cols-2 gap-2">
                         @php
                             // Loop through rows.
                             while ( have_rows('sizzle_reel') ) : the_row();
@@ -97,6 +114,67 @@ if( have_rows('sizzle_reel') ):
                                             endif;
 
                         @endphp
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-alt-2 extra-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="two-col extra-gap-right">
+                    <div class="header-two blue">
+                        <h2>Let's talk about a business video production project for your
+                            accounting firm.</h2>
+                        <p>Business video production for accounting firms is the perfect compliment to your traditional process of winning new business. Video content of your partners allows prospective clients to get a real feel for them, their expertise and their skill sets.</p>
+                    </div>
+                </div>
+                <div class="two-col">
+                    <div class="video-container video-iframe">
+                        <iframe width="auto" src="https://www.youtube.com/embed/h8g9eM9Dwxw"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="wide-form-section">
+        <div class="row wide-form-wrapper">
+            <div class="peak">
+                <div class="lead-in left alt">
+                    <div class="peak-card">
+                        <div class="peak-card-top" style="background: url(@asset('images/video-production-lights-center.jpg'));
+                    background-position: center center;">
+                            <div class="peak-card-bg"></div>
+                            <div class="peak-card-title">
+                                <span>12</span>
+                                <h3>Tips for Making Your Videos Look More Professional</h3>
+                            </div>
+                        </div>
+
+                        <div class="peak-card-logo"><img
+                                    src="@asset('images/multivision-digital-video-production-logo-trans.png')"
+                                    alt="Multivision Digitial Logo"/></div>
+                    </div>
+                </div>
+                <div class="lead-in-right">
+                    <div class="wide-form">
+                        <h2 class="preheader">Download Free PDF</h2>
+                        <h3 class="follow-up smaller">Nothing comes close to the effectiveness of a well designed video
+                            communications
+                            project.</h3>
+                        <p>Improve your search engine rankings by driving and keeeping traffic on your site and
+                            generate quality leads with professional video</p>
+                        <form>
+                            <div class="form-row">
+                                <label class="two-col even-space" id="wide-form-first-name-label">
+                                    <input id="peak-email" type="email" value="" placeholder="Enter Email Address"
+                                           required/>
+                                </label>
+                                <button class="call-to-action" id="wide-form-submit">Download</button>
+                            </div
+                        </form>
                     </div>
                 </div>
             </div>
