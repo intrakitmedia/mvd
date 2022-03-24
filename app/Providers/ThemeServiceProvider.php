2b9a3075087f96b6e2c43ca375c0a7714be8c933
Philip Rudy <me@philiparudy.com>
2022-03-24 17:28:43 -0400

    fioxes and favicon

--- app/Providers/ThemeServiceProvider.php
+++ app/Providers/ThemeServiceProvider.php
@@ -43,90 +43,90 @@ class ThemeServiceProvider extends ServiceProvider {
 		<?php
 	}
 
-	public function wrap_embed( $block_content, $block ) 
-			if ( 'core/embed' === $block['blockName'] ) {
-				$block_content = '<div class="video-container video-iframe">' . $block_content . '</div>';
-			}
-
-return $block_content;
-}
-
-/**
- * Save the menu item meta
- *
- * @param int $menu_id
- * @param int $menu_item_db_id
- */
-function kia_nav_update( $menu_id, $menu_item_db_id ) {
-
-	if ( isset( $_POST['menu_item_desc'][ $menu_item_db_id ] ) ) {
-		foreach ( $_POST['menu_item_desc'] as $menu_item_id => $menu_item_desc ) {
-			$sanitized_data = sanitize_text_field( $menu_item_desc );
-			update_post_meta( $menu_item_id, '_menu_item_desc', $sanitized_data );
+	public function wrap_embed( $block_content, $block ) {
+		if ( 'core/embed' === $block['blockName'] ) {
+			$block_content = '<div class="video-container video-iframe">' . $block_content . '</div>';
 		}
-	} else {
-		delete_post_meta( $menu_item_db_id, '_menu_item_desc' );
+
+		return $block_content;
 	}
-}
 
-public
-function register_post_types() {
-	register_post_type( 'services',
-		array(
-			'labels'       => array(
-				'name'          => __( 'Services', 'textdomain' ),
-				'singular_name' => __( 'Service', 'textdomain' ),
-				'view_item'     => __( 'View Service', 'services' ),
-				'view_items'    => __( 'View Services', 'services' ),
-				'edit_item'     => __( 'Edit Service', 'services' ),
-				'all_items'     => __( 'All Services', 'services' ),
-				'add_new_item'  => __( 'Add New Service', 'services' ),
-			),
-			'public'       => true,
-			'has_archive'  => true,
-			'rewrite'      => array( 'slug' => 'services' ),
-			'show_in_rest' => true,
-			'supports'     => array( 'title', 'thumbnail' ),
-		)
-	);
+	/**
+	 * Save the menu item meta
+	 *
+	 * @param int $menu_id
+	 * @param int $menu_item_db_id
+	 */
+	function kia_nav_update( $menu_id, $menu_item_db_id ) {
 
-	register_post_type( 'portfolios',
-		array(
-			'labels'       => array(
-				'name'          => __( 'Portfolio', 'textdomain' ),
-				'singular_name' => __( 'Portfolio', 'textdomain' ),
-				'view_item'     => __( 'View Portfolio', 'portfolios' ),
-				'view_items'    => __( 'View Portfolio', 'portfolios' ),
-				'edit_item'     => __( 'Edit Portfolio', 'portfolios' ),
-				'all_items'     => __( 'All Portfolio Items', 'portfolios' ),
-				'add_new_item'  => __( 'Add New Portfolio', 'portfolios' ),
-			),
-			'public'       => true,
-			'has_archive'  => true,
-			'rewrite'      => array( 'slug' => 'portfolio' ),
-			'show_in_rest' => true,
-			'supports'     => array( 'title', 'thumbnail' ),
-		)
-	);
+		if ( isset( $_POST['menu_item_desc'][ $menu_item_db_id ] ) ) {
+			foreach ( $_POST['menu_item_desc'] as $menu_item_id => $menu_item_desc ) {
+				$sanitized_data = sanitize_text_field( $menu_item_desc );
+				update_post_meta( $menu_item_id, '_menu_item_desc', $sanitized_data );
+			}
+		} else {
+			delete_post_meta( $menu_item_db_id, '_menu_item_desc' );
+		}
+	}
 
-	register_post_type( 'case_studies',
-		array(
-			'labels'       => array(
-				'name'          => __( 'Case Studies', 'textdomain' ),
-				'singular_name' => __( 'Case Study', 'textdomain' ),
-				'view_item'     => __( 'View Case Study', 'portfolios' ),
-				'view_items'    => __( 'View Case Studies', 'case_studies' ),
-				'edit_item'     => __( 'Edit Case Study', 'case_studies' ),
-				'all_items'     => __( 'All Case Studies', 'case_studies' ),
-				'add_new_item'  => __( 'Add New Case Study', 'case_studies' ),
-			),
-			'public'       => true,
-			'has_archive'  => true,
-			'rewrite'      => array( 'slug' => 'case-studies' ),
-			'show_in_rest' => true,
-			'supports'     => array( 'title', 'thumbnail', 'categories', 'editor' ),
-		)
-	);
+	public
+	function register_post_types() {
+		register_post_type( 'services',
+			array(
+				'labels'       => array(
+					'name'          => __( 'Services', 'textdomain' ),
+					'singular_name' => __( 'Service', 'textdomain' ),
+					'view_item'     => __( 'View Service', 'services' ),
+					'view_items'    => __( 'View Services', 'services' ),
+					'edit_item'     => __( 'Edit Service', 'services' ),
+					'all_items'     => __( 'All Services', 'services' ),
+					'add_new_item'  => __( 'Add New Service', 'services' ),
+				),
+				'public'       => true,
+				'has_archive'  => true,
+				'rewrite'      => array( 'slug' => 'services' ),
+				'show_in_rest' => true,
+				'supports'     => array( 'title', 'thumbnail' ),
+			)
+		);
+
+		register_post_type( 'portfolios',
+			array(
+				'labels'       => array(
+					'name'          => __( 'Portfolio', 'textdomain' ),
+					'singular_name' => __( 'Portfolio', 'textdomain' ),
+					'view_item'     => __( 'View Portfolio', 'portfolios' ),
+					'view_items'    => __( 'View Portfolio', 'portfolios' ),
+					'edit_item'     => __( 'Edit Portfolio', 'portfolios' ),
+					'all_items'     => __( 'All Portfolio Items', 'portfolios' ),
+					'add_new_item'  => __( 'Add New Portfolio', 'portfolios' ),
+				),
+				'public'       => true,
+				'has_archive'  => true,
+				'rewrite'      => array( 'slug' => 'portfolio' ),
+				'show_in_rest' => true,
+				'supports'     => array( 'title', 'thumbnail' ),
+			)
+		);
+
+		register_post_type( 'case_studies',
+			array(
+				'labels'       => array(
+					'name'          => __( 'Case Studies', 'textdomain' ),
+					'singular_name' => __( 'Case Study', 'textdomain' ),
+					'view_item'     => __( 'View Case Study', 'portfolios' ),
+					'view_items'    => __( 'View Case Studies', 'case_studies' ),
+					'edit_item'     => __( 'Edit Case Study', 'case_studies' ),
+					'all_items'     => __( 'All Case Studies', 'case_studies' ),
+					'add_new_item'  => __( 'Add New Case Study', 'case_studies' ),
+				),
+				'public'       => true,
+				'has_archive'  => true,
+				'rewrite'      => array( 'slug' => 'case-studies' ),
+				'show_in_rest' => true,
+				'supports'     => array( 'title', 'thumbnail', 'categories', 'editor' ),
+			)
+		);
 
 //		register_post_type('videos',
 //			array(
@@ -147,5 +147,5 @@ function register_post_types() {
 //				'supports' => array('title', 'thumbnail'),
 //			)
 //		);
-}
+	}
 }
