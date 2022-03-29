49dae7fe6c4b7d0d20386a5f51804c9f314679ce
Philip Rudy <me@philiparudy.com>
2022-03-29 18:35:18 -0400

    show

--- resources/views/template-solutions.blade.php
+++ resources/views/template-solutions.blade.php
@@ -181,8 +181,214 @@ $query = new \WP_Query( $args );
     </section>
 @endif
 
+    @php
+    $show_service = get_field('show_services');
+@endphp
 
+    @if($show_service)
 
+        <section class="section block-services section-alt-2">
+            <div class="block-services-inner">
+
+                <div class="row center">
+                    <div class="lead-in">
+                        <h3>Explore our services for your business solutions</h3>
+                        <h4 class="header-alt">Cost-effective Corporate Video Production Services</h4>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-four services-block">
+                        <div class="services-block-inner">
+                            <div class="block-flip">
+                                @php
+                                    $page = get_page_by_title('Video Strategy', 'object', 'services');
+                                @endphp
+
+                                @if($page)
+                                    @php
+                                        $link = get_permalink($page->ID);
+                                    @endphp
+                                @else
+                                    @php
+                                        $link = "#";
+                                    @endphp
+                                @endif
+                                <a title="{{$page->post_title}}" href="{{$link}}" class="block-link"
+                                   style="background: url(@asset('images/video-strategy.jpg')); background-size:
+                                   cover;">
+                                    <div class="block-flip-inner">
+                                        <div class="block-content">
+                                            <div class="block-header">
+                                                <h4 class="initial">Video Strategy</h4>
+                                            </div>
+                                            <div class="block-text">
+                                                <div class="block-text-inner">
+                                                    <h4>Video Strategy</h4>
+
+                                                    <p>Our holistic video marketing strategy lets you plan, budget and
+                                                        integrate all the
+                                                        different types video you need.</p>
+                                                    <p><span class="learn-more">Learn More</span></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-four services-block">
+                        <div class="services-block-inner">
+                            <div class="block-flip">
+                                @php
+                                    $page = get_page_by_title('Video Production', 'object', 'services');
+                                @endphp
+
+                                @if($page)
+                                    @php
+                                        $link = get_permalink($page->ID);
+                                    @endphp
+                                @else
+                                    @php
+                                        $link = "#";
+                                    @endphp
+                                @endif
+                                <a title="{{$page->post_title}}" href="{{$link}}" class="block-link"
+                                   style="background: url(@asset('images/video-production.jpg')); background-size: cover;">
+                                    <div class="block-flip-inner">
+                                        <div class="block-content">
+                                            <div class="block-header">
+                                                <h4 class="initial">Video Production</h4>
+                                            </div>
+                                            <div class="block-text">
+                                                <div class="block-text-inner">
+                                                    <h4>Video Production</h4>
+
+                                                    <p>Everything you need from the production skills to the
+                                                        marketing skills to the technical skill sets to deliver the
+                                                        final video product. </p>
+                                                    <p><span class="learn-more">Learn More</span></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-four services-block">
+                        <div class="services-block-inner">
+                            <div class="block-flip">
+                                @php
+                                    $page = get_page_by_title('Animation/Motion Graphics', 'object', 'services');
+                                @endphp
+
+                                @if($page)
+                                    @php
+                                        $link = get_permalink($page->ID);
+                                    @endphp
+                                @else
+                                    @php
+                                        $link = "#";
+                                    @endphp
+                                @endif
+                                <a title="{{$page->post_title}}" href="{{$link}}" class="block-link"
+                                   style="background: url(@asset('images/video-marketing.jpg')); background-size: cover;">
+                                    <div class="block-flip-inner">
+                                        <div class="block-content">
+                                            <div class="block-header">
+                                                <h4 class="initial">Animation</h4>
+                                            </div>
+                                            <div class="block-text">
+                                                <div class="block-text-inner">
+                                                    <h4>Animation</h4>
+
+                                                    <p>Animated marketing videos are a very effective tool for lead
+                                                        generation and conversion. </p>
+                                                    <p><span class="learn-more">Learn More</span></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-four services-block">
+                        <div class="services-block-inner">
+                            <div class="block-flip">
+                                @php
+                                    $page = get_page_by_title('Video Marketing', 'object', 'services');
+                                @endphp
+
+                                @if($page)
+                                    @php
+                                        $link = get_permalink($page->ID);
+                                    @endphp
+                                @else
+                                    @php
+                                        $link = "#";
+                                    @endphp
+                                @endif
+                                <a title="{{$page->post_title}}" href="{{$link}}" class="block-link"
+                                   style="background: url(@asset('images/animation-studio.jpg')); background-size: cover;">
+                                    <div class="block-flip-inner">
+                                        <div class="block-content">
+                                            <div class="block-header">
+                                                <h4 class="initial">Video Marketing</h4>
+                                            </div>
+                                            <div class="block-text">
+                                                <div class="block-text-inner">
+                                                    <h4>Video Marketing</h4>
+
+                                                    <p>Our holistic video marketing strategy lets you plan, budget and
+                                                        integrate all the
+                                                        different types video you need.</p>
+                                                    <p><span class="learn-more">Learn More</span></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row center">
+                    <div class="lead-out">
+                        <div class="call-to-actions-group">
+                            <div class="cta-button-wrapper js-watch">
+                                @php
+                                    if($contact_us_link) {
+                                        $link = $contact_us_link;
+                                    } else {
+                                    $link =  '#';
+                                    }
+                                @endphp
+                                <a class="call-to-action" title="Contact Us" href="{{$link}}">Contact Us</a>
+                            </div>
+                            <div class="cta-button-wrapper js-watch">
+                                @php
+                                    $case_studies_link = get_post_type_archive_link('case_studies');
+                                        if($contact_us_link) {
+                                            $link = $case_studies_link;
+                                        } else {
+                                        $link =  '#';
+                                        }
+                                @endphp
+                                <a class="call-to-action btn-alt-2" title="Case Studies" href="{{$link}}">View Customer
+                                    Case
+                                    Studies</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        @endif
 
     <section class="services-sizzle section-alt">
         <div class="container">
