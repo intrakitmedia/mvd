62c023fef2523a27182caa99127732751751cbd9
Philip Rudy <me@philiparudy.com>
2022-04-04 16:13:53 -0400

    add args

--- app/filters.php
+++ app/filters.php
@@ -15,10 +15,10 @@ add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
 
-add_filter('wp_trim_words', function ($text, $num_words, $more, $original_text){
+add_filter('wp_trim_words', function ($text, $num_words, $more, $original_text) {
 	if (! empty($text)) {
 		return $text = preg_replace('~(?:http|https|)(?::\/\/|)(?:www.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{11})[a-z0-9;:@?&%=+\/\$_.-]*~i', '', $text);
 	}
 
 	return $text;
-});
+}, 10, 4);
