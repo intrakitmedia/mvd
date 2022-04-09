2d76a7d3e18ef4bde169105e6e18411276552ed4
Philip Rudy <me@philiparudy.com>
2022-04-09 18:58:19 -0400

    hi

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -86,13 +86,6 @@ domReady(() => {
         $megaMenu.toggleClass('mobile-open');
     })
 
-
-
-    $('.pdf-dl').on('click', function(e) {
-        e.preventDefault();
-
-    });
-
     new Video();
 });
 
