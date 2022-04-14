5516b337c719663002fb586c942954a9ccbe2a5f
Philip Rudy <me@philiparudy.com>
2022-04-13 23:11:40 -0400

    transition

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -86,6 +86,16 @@ domReady(() => {
         $megaMenu.toggleClass('mobile-open');
     })
 
+    var num = 200; //number of pixels before modifying styles
+
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > num) {
+            $('.menu').addClass('fixed');
+        } else {
+            $('.menu').removeClass('fixed');
+        }
+    });
+
     new Video();
 });
 
