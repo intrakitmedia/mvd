df92ffdba9e2e2aa3c7e18759345a1498c56cf44
Philip Rudy <me@philiparudy.com>
2022-04-15 18:48:07 -0400

    portfolio videos

--- resources/views/archive-portfolios.blade.php
+++ resources/views/archive-portfolios.blade.php
@@ -16,10 +16,10 @@
 
 @section('content')
 
-    <section class="parent">
+    <section class="parent the-page-header">
         <div class="container">
             <div class="row center">
-                <div class="page-header-title blog-page-header text-center">
+                <div class="page-header-title  blog-page-header text-center">
                     <h1>Portfolio</h1>
                     <h2>Over 1000+ business videos produced for our clients</h2>
                 </div>
@@ -30,7 +30,10 @@
     <section class="serie-section page-header">
         <div class="container">
             <div class="row center">
-                <div class="services">
+                <div class="portfolio-archive grid sm:grid-cols-1 md:grid-cols-3 gap-6">
+
+                    @include('components.play-circle')
+
                     @if($query->found_posts)
                         @foreach($posts as $post)
                             @php
@@ -38,12 +41,21 @@
                                 $post_link = get_permalink($post->ID);
 
                             $banner_bold = get_field('banner_bold_content', $post->ID);
+
+                            $video = get_field('featured_video_object', $post->ID);
+                                        if($video) {
+                                        $youtube_id = get_field('youtube_id', $video->ID);
+                                        $youtube_id = preg_replace('/\s+/', '', $youtube_id);
+                                        }
                             @endphp
-                            <div class="service text-center">
+                            <div class="text-center">
+                                @if($video && $youtube_id)
+                                    @include('components.s-vid-no-title')
+                                @endif
                                 <div class="service-title">
-                                    <h2 class="blue"><a title="{!! $post->post_title !!}"
+                                    <h3 class="blue"><a title="{!! $post->post_title !!}"
                                                         href="{{$post_link}}">{!! $post->post_title
-                                 !!}</a></h2>
+                                 !!}</a></h3>
                                     <p>{!! $banner_bold !!}</p>
 
                                     <a title="{!! $post->post_title !!}" href="{{$post_link}}"
@@ -53,6 +65,7 @@
                         @endforeach
                     @endif
                 </div>
+                @include('components.modal')
             </div>
         </div>
     </section>
---
+++ resources/views/components/s-vid-no-title.blade.php
@@ -0,0 +1,19 @@
+
+<div data-field="{{$youtube_id}}" class="s-vid">
+    <div class="s-vid-inner">
+        <div class="video-img-container video-container video-thumbnail"
+             style="background-image: url('<?php echo "https://img.youtube.com/vi/$youtube_id/maxresdefault.jpg"; ?>');
+                     background-size: 103%;
+                     background-position: -2px;">
+            <div class="abso-bg"></div>
+            <div class="player-button-container">
+                <div class="player-button">
+                    <svg aria-hidden="true" focusable="false">
+                        <use xlink:href="#plyr-play"></use>
+                    </svg>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
