6f8d140b47455754b29f8a5ac321fd793c62ce89
Philip Rudy <me@philiparudy.com>
2022-04-15 14:04:16 -0400

    lead gen

--- resources/views/template-about-us.blade.php
+++ resources/views/template-about-us.blade.php
@@ -259,47 +259,7 @@ src="@asset('images/Accredited-Agency-2022.jpeg')" alt="Top 10 Corporate Video P
         </div>
     </section>
 
-    <section class="wide-form-section section-alt-2">
-        <div class="row wide-form-wrapper">
-            <div class="peak">
-                <div class="lead-in left alt">
-                    <div class="peak-card">
-                        <div class="peak-card-top" style="background: url(@asset('images/video-production-lights-center.jpg'));
-                    background-position: center center;">
-                            <div class="peak-card-bg"></div>
-                            <div class="peak-card-title">
-                                <span>12</span>
-                                <h3>Tips for Making Your Videos Look More Professional</h3>
-                            </div>
-                        </div>
-
-                        <div class="peak-card-logo"><img
-                                    src="@asset('images/multivision-digital-video-production-logo-trans.png')"
-                                    alt="Multivision Digitial Logo"/></div>
-                    </div>
-                </div>
-                <div class="lead-in-right">
-                    <div class="wide-form">
-                        <h2 class="preheader">Download Free PDF</h2>
-                        <h3 class="follow-up smaller">Nothing comes close to the effectiveness of a well designed video
-                            communications
-                            project.</h3>
-                        <p>Improve your search engine rankings by driving and keeeping traffic on your site and
-                            generate quality leads with professional video</p>
-                        <form>
-                            <div class="form-row">
-                                <label class="two-col even-space" id="wide-form-first-name-label">
-                                    <input id="peak-email" type="email" value="" placeholder="Enter Email Address"
-                                           required/>
-                                </label>
-                                <button class="call-to-action" id="wide-form-submit">Download</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('components.lead-gen')
 
 @endsection
 
