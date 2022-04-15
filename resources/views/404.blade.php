579369bb36dc548a7e724d823471f311a828bbf1
Philip Rudy <me@philiparudy.com>
2022-04-15 10:27:08 -0400

    changes

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,26 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
 
   @if (! have_posts())
-    <div class="four-oh-four">
 
+      <section class="section-alt-2">
 
-    <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-    </x-alert>
-
-      <div class="f-num">
-        <span>404</span>
-      </div>
-
-      <div class="not-found">
-        {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-
-        <a href="{{  get_home_url() }}" class="call-to-action">Home</a>
-      </div>
-
+<div class="container">
+  <div class="row center">
+    <div class="f-num">
+      <span>404</span>
     </div>
+  </div>
+
+  <div class="f-num">
+
+
+
+  <div class="not-found">
+      <p>Sorry, but the page you are trying to view does not exist.</p>
+
+      <a href="{{  get_home_url() }}" class="call-to-action">Home</a>
+    </div>
+  </div>
+</div>
+
+
+
+      </section>
+
+
   @endif
 @endsection
