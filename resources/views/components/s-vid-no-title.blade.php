a917f94582a3baf1d7fb63b5bf55bed4c6c2962d
Philip Rudy <me@philiparudy.com>
2022-04-16 08:57:24 -0400

    featurd image for videos

--- resources/views/components/s-vid-no-title.blade.php
+++ resources/views/components/s-vid-no-title.blade.php
@@ -1,8 +1,12 @@
+@php
+$image = get_the_post_thumbnail_url($video->ID) ?? null;
+$image = $image ?? "https://img.youtube.com/vi/$youtube_id/maxresdefault.jpg";
+@endphp
 
 <div data-field="{{$youtube_id}}" class="s-vid">
     <div class="s-vid-inner">
         <div class="video-img-container video-container video-thumbnail"
-             style="background-image: url('<?php echo "https://img.youtube.com/vi/$youtube_id/maxresdefault.jpg"; ?>');
+             style="background-image: url('{{$image}}');
                      background-size: 103%;
                      background-position: -2px;">
             <div class="abso-bg"></div>
