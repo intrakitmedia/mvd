4d745af0ffacaca41f30eb3012ca22a8208efa05
Philip Rudy <me@philiparudy.com>
2022-05-03 21:05:51 -0400

    offset of 2 now

--- app/View/Composers/BlogComposer.php
+++ app/View/Composers/BlogComposer.php
@@ -75,6 +75,7 @@ class BlogComposer extends Composer {
 			'post_type'      => $post_type,
 			'post_status'         => 'publish',
 			'posts_per_page' => 12,
+			'offset' => 2,
 			'paged' => $paged
 		];
 
