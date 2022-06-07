c76bc0957266740fd5e66e7560ff1e1b882e5666
Philip Rudy <me@philiparudy.com>
2022-06-07 18:47:09 -0400

    remeove whitespace

--- resources/views/template-about-us.blade.php
+++ resources/views/template-about-us.blade.php
@@ -248,16 +248,17 @@
             <div class="accolades-img">
 
                 <a title="Top 10 Corporate Video Production Companies in New York" href="https://giggster.com/blog/corporate-video-production-companies-new-york/">
-                    <img src="@asset('images/giggster-corporate-2021.jpg')" alt="Top 10 Corporate Video ProductionCompanies in New York" /></a>
+                    <img src="@asset('images/giggster-corporate-2021.jpg')" alt="Top 10 Corporate Video ProductionCompanies in New York" />
+                </a>
                 <a title="Top NYC Video Production Companies" href="https://www.designrush.com/agency/video-production/new-york/new-york-city">
-                    <img src="@asset('images/Accredited-Agency-2022.jpeg')" alt="Top 10 Corporate Video Production Companies in New York" /></a>
-                <a title="Verified Agency 2022" href="https://agencyvista
-                .com/agency/multivision-digital/marketing-agency-new-york-new-york-us">
-                    <img src="@asset('images/verified-agency-vista-badge.png')" alt="Verified Agency 2022"/></a>
+                    <img src="@asset('images/Accredited-Agency-2022.jpeg')" alt="Top 10 Corporate Video Production Companies in New York" />
+                </a>
+                <a title="Verified Agency 2022" href="https://agencyvista.com/agency/multivision-digital/marketing-agency-new-york-new-york-us">
+                    <img src="@asset('images/verified-agency-vista-badge.png')" alt="Verified Agency 2022"/>
+                </a>
                 <a href="https://www.expertise.com/ny/nyc/law-firm-marketing" style="display:inline-block; border:0;">
-                    <img src="https://res.cloudinary.com/expertise-com/image/upload/f_auto,fl_lossy,
-                    q_auto/w_auto/remote_media/awards/ny_nyc_law-firm-marketing_2022_transparent.svg" alt="Top
-                    Legalgit s Marketing Company in New York City" /></a>
+                    <img src="https://res.cloudinary.com/expertise-com/image/upload/f_auto,fl_lossy,q_auto/w_auto/remote_media/awards/ny_nyc_law-firm-marketing_2022_transparent.svg" alt="Top Legalgit s Marketing Company in New York City" />
+                </a>
             </div>
         </div>
         </div>
